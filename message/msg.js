fd51d762edf4a9ae1b381b4703b14fe2fd1073ee
WanX123 <88576572+WanX123@users.noreply.github.com>
2022-01-23 07:58:32 +0700

    Update msg.js

--- message/msg.js
+++ message/msg.js
@@ -319,7 +319,7 @@ conn.sendMessage(from, {
 		switch(command) {
 case prefix+'tr': case prefix+'translate': 
 if (!q) return reply(`Contoh:\n${command} [Kode Bahasa] house\nKode bahasa bisa cek di https://cloud.google.com/translate/docs/languages`)
-kode_negara = args.join(" ")
+kode_negara = q
 teks = msg.message.extendedTextMessage.contextInfo.quotedMessage.conversation
 translate(`${teks}`,{to:`${kode_negara}`}).then( res => {
 ini_txt = `*Translate*
